b2ea34747bd36396484ecd51b39e7c7de12cd52c
Shane Levine <slev12397@gmail.com>
2017-10-15 17:13:20 -0400

    Finished HW6 part b

--- HW6/HW6.py
+++ HW6/HW6.py
@@ -1,6 +1,8 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import re
+
+# PART A
 url = 'http://py4e-data.dr-chuck.net/comments_36400.html'
 html = urllib.request.urlopen(url).read()
 
@@ -9,3 +11,18 @@ nums = []
 for each in soup.find_all('span'):
     nums.append(int(each.text))
 print(sum(nums))
+
+# PART B
+def follow(a,b):
+    name=[]
+    url = 'http://py4e-data.dr-chuck.net/known_by_Marty.html'
+    for x in range(a):
+        html = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(html, 'lxml')
+        q = soup.find_all('a')
+        pick = q[(b-1)]
+        url = pick['href']
+        name.append(pick.text)
+    print(name[-1])
+
+follow(7,18)
