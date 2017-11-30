90542be285a16147295c12b8a565dd652f95c7e6
Shane Levine <slev12397@gmail.com>
2017-11-30 00:00:07 -0500

    working

--- final_project/final.py
+++ final_project/final.py
@@ -3,3 +3,10 @@ import json
 import matplotlib.pyplot as plt
 import time
 from matplotlib import style
+import aiml
+import os
+
+kernel = aiml.Kernel()
+
+for f in os.listdir('aiml_data'):
+    kernel.learn(os.path.join('aiml_data', f))
