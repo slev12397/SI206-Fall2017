f1e606f51aec9f1865389989391f4b19d5b26feb
Shane Levine <slev12397@gmail.com>
2017-12-06 16:57:03 -0500

    final project working

--- final_project/final.py
+++ final_project/final.py
@@ -3,10 +3,27 @@ import json
 import matplotlib.pyplot as plt
 import time
 from matplotlib import style
-import aiml
-import os
+import instagram_info
+import spotipy
+import sys
+import spotipy.util as util
 
-kernel = aiml.Kernel()
 
-for f in os.listdir('aiml_data'):
-    kernel.learn(os.path.join('aiml_data', f))
+
+def instagram_collector():
+    access_token = instagram_info.access_token
+    client_id = instagram_info.client_id
+    url = 'https://api.instagram.com/v1/users/self/media/liked?access_token='+ access_token
+    response = requests.get(url)
+    return json.loads(response.text)
+def spotify_collector():
+    spotify = spotipy.Spotify()
+    util.prompt_for_user_token('1447449240','user-read-recently-played',client_id='76422d4dd50d44469f327b071520f5a7',client_secret='f2b6931f6cc441a09894d84790759c75',redirect_uri='https://localhost:3000/callback')
+    return spotify.current_user_recently_played(limit=50)
+
+def youtube_collector():
+    pass
+sp = spotipy.Spotify()
+
+results = sp.search(q='weezer', limit=20)
+print(results)
