0556771a2c82548919dbb763b364766d6020b3a1
Shane Levine <slev12397@gmail.com>
2017-12-08 17:16:22 -0500

    finished spotify api

--- final_project/final.py
+++ final_project/final.py
@@ -8,7 +8,8 @@ import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 import pprint
 import spotipy
-import spotipy
+from datetime import date
+import calendar, datetime
 import sqlite3
 
 CACHE_FNAME = "finalproject_cache.json"
@@ -46,23 +47,37 @@ def get_saved_songs(username):
         print("Wasn't in cache and wasn't valid search either")
         return None
 def youtube_watch_history(username):
-    api_key = 'AIzaSyCvjekk8uxhz2MUbNbo6KVl3ZUXVrEDEeE'
+    api_key = 'AIzaSyDYEsSZlbn_yIBv8iLUydXPFgObOlSTVGE'
+    # UC3cUJuFy0xws-AGsU3sqR9w
+    parameters = {'part':'snippet,contentDetails',
+    "key":api_key,
+    'maxResults': '50',
+    'playlistId':'LL3cUJuFy0xws-AGsU3sqR9w',
+    'channelId': 'UC3cUJuFy0xws-AGsU3sqR9w'
+    }
+    url = 'https://www.googleapis.com/youtube/v3/playlists'
+    res = requests.request(method="get", url=url, params=parameters)
+    results = json.loads(res.text)
+    return results
 
 results = get_saved_songs('slev12397')
 songs_dict = {}
 for track in results['items']:
     time = track['track']["duration_ms"]
     song_id = track['track']['id']
-    songs_dict[track['track']['name']] = [time, song_id]
+    time_added = track['added_at']
+    day_added = date(int(time_added[0:4]),int(time_added[5:7]),int(time_added[8:10])).weekday()
+    day = calendar.day_name[day_added]
+    songs_dict[track['track']['name']] = [time, song_id, day]
 
 conn = sqlite3.connect('final_project.sqlite')
 cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS Songs')
-cur.execute('CREATE TABLE Songs(song_id NUMBER, song_name TEXT, duration NUMBER)')
+cur.execute('CREATE TABLE Songs(song_id NUMBER, song_name TEXT, duration NUMBER, day_added TEXT)')
 for song in songs_dict:
-    tup = songs_dict[song][1],song, songs_dict[song][0]
-    cur.execute('INSERT INTO Songs (song_id, song_name, duration) VALUES (?,?,?)',tup)
+    tup = songs_dict[song][1],song, songs_dict[song][0], songs_dict[song][2]
+    cur.execute('INSERT INTO Songs (song_id, song_name, duration,day_added) VALUES (?,?,?,?)',tup)
 
 conn.commit()
 cur.close()
@@ -70,7 +85,18 @@ song_duration ={}
 for track in songs_dict:
     song_duration[track] = songs_dict[track][0]
 
-plt.bar(range(len(song_duration)), song_duration.values(), align='center')
-plt.xticks(range(len(song_duration)), list(song_duration.keys()))
+# plt.bar(range(len(song_duration)), song_duration.values(), align='center')
+# plt.xticks(range(len(song_duration)), list(song_duration.keys()))
 
-# plt.show()
+weekday_feq = {}
+for track in songs_dict:
+    if songs_dict[track][2] in weekday_feq:
+        weekday_feq[songs_dict[track][2]] +=1
+    else:
+        weekday_feq[songs_dict[track][2]]=1
+for day in weekday_feq:
+    print("On " + day + 's I added ' + str(weekday_feq[day]) + ' songs to my Spotify library')
+    print('============================================================')
+plt.bar(range(len(weekday_feq)), weekday_feq.values(), align='center')
+plt.xticks(range(len(weekday_feq)), list(weekday_feq.keys()))
+plt.show()
